26ca297273a0a3f5f40c24d2c0ae9ca8d722eeae
Jinnnyjin <jinnnny.jin@gmail.com>
2026-08-20 09:38:13 +0900

    [Add] Programmers Lv.1 내적

---
+++ Programmers/Lv.1/0819 내적/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _0819_내적
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4 };
+            int[] b = { -3, -1, 0, 2 };
+
+            int answer = solution(a, b);
+            Console.WriteLine(answer);
+        }
+
+        public static int solution(int[] a, int[] b)
+        {
+            int answer = 0;
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                answer += a[i] * b[i];
+            }
+
+            return answer;
+        }
+    }
+}
