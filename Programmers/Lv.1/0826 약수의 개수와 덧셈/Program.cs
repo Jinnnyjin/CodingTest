b569633e778e285caa62f7621d847c4b41a81696
Jinnnyjin <jinnnny.jin@gmail.com>
2026-08-26 09:52:11 +0900

    Add] Programmers Lv.1 약수의 개수와 덧셈

---
+++ Programmers/Lv.1/0826 약수의 개수와 덧셈/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _0826_약수의_개수와_덧셈
+{
+    /*
+     * 문제 설명
+    두 정수 left와 right가 매개변수로 주어집니다.
+    left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고,
+    약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+
+    제한사항
+    1 ≤ left ≤ right ≤ 1,000
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(24, 27));
+        }
+
+
+        public static int solution(int left, int right)
+        {
+            int answer = 0;
+
+            for(int i = left; i <= right; i++ )
+            {
+                double tmp = Math.Sqrt(i);
+
+                if (tmp - (int)tmp == 0)
+                {
+                    answer -= i;
+                }
+                else
+                    answer += i;
+            }
+
+
+            return answer;
+
+        }
+    }
+}
